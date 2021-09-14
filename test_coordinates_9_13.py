7d370a64e4e8606505a511bd9b8ab83f4ab22557
Celina <celinazhou27@gmail.com>
2021-09-14 14:50:17 -0400

    Changed name of function file to match file containing function to allow pytest to run

--- test_coordinates_9_13.py
+++ test_coordinates_9_13.py
@@ -1,6 +1,6 @@
 # test_coordinates_9_13.py
 def test_coordinateline():
-    from coordinates import coordinateline
+    from coordinates_9_13 import coordinateline
     result = coordinateline((0,0),(2,2),1)
     expected = 1
     assert result == expected
